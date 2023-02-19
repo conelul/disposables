d56dc0c4a1ca9f9090e2293fd6b648d7dcabfa1a
Conor Scheidt <conorrs@protonmail.com>
2023-02-19 17:14:43 -0600

    Fully automate

--- server/create.py
+++ server/create.py
@@ -3,19 +3,34 @@ import argparse
 import json
 from itertools import islice
 import boto3
+import sys
 import time
 
 REGION: str = "us-east-2"
 SRC_REPO = "https://github.com/conelul/disposables"
+NUM_TRIALS = 3
+NUM_TABS = 20
+LOAD_TIME_MS = 15000
+DB_URI = "mongodb://root:pass1234@disposablesdb.c39d9rmhj8ez.us-east-2.docdb.amazonaws.com:27017/?retryWrites=false"
 
 
-def chunks(data: dict[str, list[str]], size):
-    it = iter(data)
-    for i in range(0, len(data), size):
-        yield {k: data[k] for k in islice(it, size)}
+def splita(a: list, n: int):
+    k, m = divmod(len(a), n)
+    return (a[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
 
 
-# def run_server(client, input: dict[str, list[str]]) -> dict:
+def split(d: dict, n: int):
+    l = []
+    for (k, a) in d.items():
+        for v in a:
+            l.append((k, v))
+    res = []
+    for ch in splita(l, n):
+        d = {}
+        for (k, v) in ch:
+            d.setdefault(k, []).append(v)
+        res.append(d)
+    return res
 
 
 def main() -> None:
@@ -27,60 +42,56 @@ def main() -> None:
         help="Number of servers to shard collection over",
         default=1,
     )
-    # parser.add_argument("input")
+    parser.add_argument("input")
     args = parser.parse_args()
     #! Load input
-    # with open(args.input) as f:
-    #     sites = json.load(f)
-        # input_name = f.name
+    with open(args.input) as f:
+        sites = json.load(f)
+        chunks = split(sites, int(args.num_servers))
     #! Deploy
-    # ssm = boto3.client("ssm", region_name=REGION)
+    ssm = boto3.client("ssm", region_name=REGION)
     ec2 = boto3.client("ec2", region_name=REGION)
-    # waiter = ec2.get_waiter('instance_status_ok')
+    waiter = ec2.get_waiter("instance_status_ok")
     # Config to install SSM
     init = f"""#cloud-config
 repo_update: true
 repo_upgrade: all
 
 runcmd:
-    - export HOME=/home/ec2-user
     - sudo amazon-linux-extras install epel -y
     - sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
     - sudo yum install -y git chromium gcc
-    - sudo chown -R ec2-user /home/ec2-user/
-    - curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
     - cd /home/ec2-user/
-    - git clone {SRC_REPO} disposables && cd disposables/collection
-    - wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+    - wget https://github.com/conelul/disposables/releases/download/collection/collection
+    - sudo chmod 777 collection
     
 """
     # Create servers
     servers = ec2.run_instances(
         LaunchTemplate={"LaunchTemplateId": "lt-02c15b50d3a70c40f"},
         UserData=init,
-        # SecurityGroupIds=["sg-0816ba1f92424b2f6"],
         MinCount=int(args.num_servers),
         MaxCount=int(args.num_servers),
     )
     instance_ids = [instance["InstanceId"] for instance in servers["Instances"]]
     print(f"instance ids: {instance_ids}")
-    # print("waiting for server init")
-    # waiter.wait(InstanceIds=instance_ids)
-    # input("press enter when the servers are ready")
-    # for (instance, inp) in zip(servers["Instances"], chunks(sites, args.num_servers)):
-    #     cmds = [
-    #         f"echo {str(inp)} > input.json",
-    #         f"git clone {SRC_REPO} -o src",
-    #         f"cd src/collection && go mod tidy && go run main.go > log.txt",
-    #     ]
-    #     resp = ssm.send_command(
-    #         InstanceIds=[instance["InstanceId"]],
-    #         DocumentName="AWS-RunShellScript",
-    #         Parameters={"commands": cmds},
-    #     )
+    print("waiting for server init")
+    waiter.wait(InstanceIds=instance_ids)
+    print("waiting three minutes for cloud-config")
+    time.sleep(2.5 * 60)
+    print("finished waiting")
+    for (instance, inp) in zip(servers["Instances"], chunks):
+        id = instance["InstanceId"]
+        cmds = [
+            f"echo '{str(inp)}' > /home/ec2-user/input.json",
+            f"cd /home/ec2-user && for ((i = 1 ; i <= {NUM_TRIALS} ; i++ )); do RUST_LOG=info ./collection --trial $i --server-id {id} --tabs {NUM_TABS} --load-time-ms {LOAD_TIME_MS} --input input.json --db-uri {DB_URI} --db-name disposables-proof --coll-name sites50 > $i.log 2>&1; done",
+        ]
+        resp = ssm.send_command(
+            InstanceIds=[id],
+            DocumentName="AWS-RunShellScript",
+            Parameters={"commands": cmds},
+        )
 
-
-# Clean input - run over 10 servers (~2k each) and then meet
 
 if __name__ == "__main__":
     main()
